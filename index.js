4105ea3e5e7f20d9e976c19000411d7967f7967c
Mar <mar@strawmelonjuice.com>
2023-10-31 14:39:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,15 +12,13 @@ module.exports = {
 			const sshell = (cmd) => execSync(cmd, { encoding: 'utf8', cwd: join(__dirname, "/../../") });
 			const ashell = (cmd) => exec(cmd, { encoding: 'utf8', cwd: join(__dirname, "/../../") });
 			if (config.PullonStarts) {
-				console.log("Git-Puller\t\tRunning: git fetch");
+				console.log("Git-Puller\t\tFetching and pulling changes...");
 				sshell("git fetch");
-				console.log("Git-Puller\t\tRunning: git pull");
 				ashell("git pull");
 			}
 			setInterval(() => {
-				console.log("Git-Puller\t\tRunning: git fetch");
+				console.log("Git-Puller\t\tFetching and pulling changes...");
 				sshell("git fetch");
-				console.log("Git-Puller\t\tRunning: git pull");
 				if (config.FullRestartOnIntervalPull) {
 					sshell("git pull");
 					process.exit(1);
